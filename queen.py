0fce1536ffa80a21d092c9675c1dd6bab5d39f50
LumiaBlue <77127581+LumiaBlue@users.noreply.github.com>
2021-12-04 15:59:53 -0500

    Basic framework for remaining pieces

---
+++ queen.py
@@ -0,0 +1,23 @@
+#Skye Smith
+#class for Queen pieces
+
+class Rook:
+    def _init_(self, color, row, col):
+        self.type = "queen"
+        self.color = color
+        self.row = row
+        self.col = col
+
+    def validate_move(self, new_row, new_col, board):
+                                    
+
+    def update(self, new_row, new_col, board):
+            board[new_row][new_col] = self
+            self.row = new_row
+            self.col = new_col
+    
+    def get_color(self):
+        return self.color
+    
+    def get_type(self):
+        return self.type
