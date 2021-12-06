06b07720e9718617b617bc6c6fdf696f03987e92
LumiaBlue <77127581+LumiaBlue@users.noreply.github.com>
2021-12-06 14:50:00 -0500

    complete pawn validation

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -18,10 +18,14 @@ class Rook:
     def validate_move(self, new_row, new_col, board):
         valid = True
 
+        #initialize add, 
+        add = 0
+        if self.color == "white":
+            add = -1
+        else:
+            add = 1
+
         if new_row == self.row and new_col == self.col:
-            valid = False
-        
-        if abs(new_row - self.row) == 2 and (self.has_moved or new_col != self.col):
             valid = False
         
         #validate a capture (diagonal move)
@@ -29,18 +33,29 @@ class Rook:
             #is new_col equivalent to self.col + or - 1
             if abs(new_col - self.col) != 1:
                 valid = False
-            #is 
-            elif abs(new_row - self.row) != 1:
+            #is change in row != to 1 or in the wrong direction
+            elif self.row + add != new_row:
                 valid = False
+            #is there no piece in the target destination
             elif board[new_row][new_col] == 0:
                 valid = False 
+            #is the piece in the target destination of the same color
             elif board[new_row][new_col].get_color == self.color:
                 valid = False
-
-
+        #first move double step
+        elif abs(new_row - self.row) == 2:
+            #is the move in the correct direction?
+            if self.row + (2 * add) != new_row:
+                valid False
+            #has the pawn moved previously?
+            elif has_moved:
+                valid = False
+        #is the move not one space towards the opposite color edge of the board?
+        elif self.row + add != new_row:
+            vaid = False
         
-
-
+        return valid
+        
     #params: new_row - int; new_col - int; board - 2D list
     #update the object values for row and column, and "move" piece from former position to new position in board and change has_moved to True
     #returns: none 
