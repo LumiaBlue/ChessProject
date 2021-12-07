e04d0a410226d1d916ddc0234b7d4de4c48e1230
imochii <95445927+imochii@users.noreply.github.com>
2021-12-07 15:08:39 -0500

    rand, king, knight

--- pieces/king.py
+++ pieces/king.py
@@ -1,23 +1,49 @@
-#Skye Smith
+#Cat Jones
 #class for Queen pieces
 
-class Rook:
+class King:
     def _init_(self, color, row, col):
-        self.type = "queen"
+        self.type = "king"
         self.color = color
         self.row = row
         self.col = col
 
     def validate_move(self, new_row, new_col, board):
-                                    
+        valid = True 
+        if self.row == new_row and self.col == new_col:
+            valid = False
+        elif self.row == new_row:
+            for i in range (self.row, new_row):
+                if
 
+
+            #difference one or zero (can't both be zero)
+        elif self.col == new_col:
+            for i in range (self.col, new_col):
+                if 
+            #difference one or zero (can't both be zero)
+
+        else:
+            valid = False
+
+        if board[new_row][new_col] != 0 and board[new_row][new_col].get_color() == self.color:
+            valid = False
+
+        return valid
+                                    
     def update(self, new_row, new_col, board):
-            board[new_row][new_col] = self
-            self.row = new_row
-            self.col = new_col
+        board[new_row][new_col] = self
+        self.row = new_row
+        self.col = new_col
     
     def get_color(self):
         return self.color
     
     def get_type(self):
         return self.type
+
+    def get_col(self):
+        return self.col
+
+    def get_row(self):
+        return self.row
--- pieces/knight.py
+++ pieces/knight.py
@@ -1,9 +1,9 @@
-#Skye Smith
+#Cat Jones
 #class for Queen pieces
 
-class Rook:
+class Knight:
     def _init_(self, color, row, col):
-        self.type = "queen"
+        self.type = "knight"
         self.color = color
         self.row = row
         self.col = col
@@ -12,9 +12,9 @@ class Rook:
                                     
 
     def update(self, new_row, new_col, board):
-            board[new_row][new_col] = self
-            self.row = new_row
-            self.col = new_col
+        board[new_row][new_col] = self
+        self.row = new_row
+        self.col = new_col
     
     def get_color(self):
         return self.color
