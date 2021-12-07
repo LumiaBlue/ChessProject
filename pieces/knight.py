601a58081d93814a2cf587c2d222b1eb3f641fd1
imochii <95445927+imochii@users.noreply.github.com>
2021-12-07 16:27:43 -0500

    KING IS DONEE

--- pieces/knight.py
+++ pieces/knight.py
@@ -9,7 +9,30 @@ class Knight:
         self.col = col
 
     def validate_move(self, new_row, new_col, board):
-                                    
+
+        valid = True 
+        #staying in the same spot is not a valid 'move'
+        if self.row == new_row and self.col == new_col:
+            valid = False
+
+        #given the 'L' shape, neither value can equal 0
+        elif abs(new_col - self.col) < 1 or abs(new_row - self.row) < 1:
+            valid = False
+        #given the 'L' shape, neither value can be more than 2 
+        elif abs(new_col - self.col) > 2 or abs(new_row - self.row) > 2:
+            valid = False
+            
+        #given the 'L' shape, it's not valid for both values to equal 1
+        elif abs(new_col - self.col) == 1 and abs(new_row - self.row) == 1:
+            valid = False
+        #given the 'L' shape, it's not valid for both value to equal 2
+        elif abs(new_col - self.col) == 2 and abs(new_row - self.row) == 2:
+            valid = False
+
+        #is target position occupied by same-color piece
+        if board[new_row][new_col] != 0 and board[new_row][new_col].get_color() == self.color:
+            valid = False
+        return valid                                    
 
     def update(self, new_row, new_col, board):
         board[new_row][new_col] = self
