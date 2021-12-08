e9c3a78bc9293689a3652b61fc8d31b61f4b97d1
imochii <95445927+imochii@users.noreply.github.com>
2021-12-08 16:25:43 -0500

    board complete

--- chess.py
+++ chess.py
@@ -1,19 +1,28 @@
-#Cat Jones
 
 from pieces import rook, bishop, queen, king, knight, pawn
 
 def main():
+
+    #Cat Jones
+    board = [[rook.Rook("black",0,0), knight.Knight("black",0,1), bishop.Bishop("black",0,2), queen.Queen("black",0,3), king.King("black",0,4), bishop.Bishop("black",0,5), knight.Knight("black",0,6), rook.Rook("black",0,7)],
+            [pawn.Pawn("black",1,0), pawn.Pawn("black",1,1), pawn.Pawn("black",1,2), pawn.Pawn("black",1,3), pawn.Pawn("black",1,4), pawn.Pawn("black",1,5), pawn.Pawn("black",1,6), pawn.Pawn("black",1,7)],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [pawn.Pawn("white",6,0), pawn.Pawn("white",6,1), pawn.Pawn("white",6,2), pawn.Pawn("white",6,3), pawn.Pawn("white",6,4), pawn.Pawn("white",6,5), pawn.Pawn("white",6,6), pawn.Pawn("white",6,7)],
+            [rook.Rook("white",7,0), knight.Knight("white",7,1), bishop.Bishop("white",7,2), queen.Queen("white",7,3), king.King("white",7,4), bishop.Bishop("white",7,5), knight.Knight("white",7,6), rook.Rook("white",7,7)]]
+
     checkmate = False 
     while checkmate = False:         
-
-        print("ahoy there matey, it's your turn!")
+        #Cat Jones
+        print("ahoy, it's your turn!")
 
         #current coordinate
         piece = input("Please enter in the current coordinates of the piece you would like to place in play (ex: 2,4): ")
         #this splits the inputted coordinates by the comma, which is there for user visual-convience
         (col,row) = piece.split(',')
 
-        #DO WORK YOU PAID FOR THIS TRAUMA
         #after asking for new move
         board[row][col].validate_move(new_row, new_col, board)
 
