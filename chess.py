f216c03f5890cc39c2ede11ed3d6851f027784fe
LumiaBlue <77127581+LumiaBlue@users.noreply.github.com>
2021-12-09 17:52:17 -0500

    ooga booga

--- chess.py
+++ chess.py
@@ -17,7 +17,7 @@ def main():
 
     #Start of the while loop, which continues under the condition that the king isn't in checkmate per the checkmate function.
     checkmate = False 
-    while checkmate = False:   
+    while checkmate == False:   
 
         #Cat Jones
         print("Ahoy, it's your turn!")
@@ -29,103 +29,104 @@ def main():
         #new move
         new_move = input("Please enter the coordinates for the space you would like to move the selected piece to (ex: 1,6): ")
         row, col = new_move_validation(new_move, piece, board)
+      
+#Cat Jones
+def captured():
 
-#-----------------------edited^^^         
+    captured_white = 0
+    captured_black = 0
 
-    #Cat Jones
-    def captured():
+    #in the main file 
+    if self.color(new_move) == other.color():
+        # if board([x][y]).get_color == white
+        captured_black = captured_black + 1
+        #cap_color undefined
+    elif cap_color == 'W' or cap_color == 'w':
+        captured_white = captured_white + 1
 
-        captured_white = 0
-        captured_black = 0
+    print(f"Captured by White: {captured_black} | Captured by Black: {captured_white}")
+    
+def current_position_validation(input, board):
+    piece = 0
+    all_valid = False
 
-        #in the main file 
-        if self.color(new_move) == other.color():
-            # if board([x][y]).get_color == white
-            captured_black = captured_black + 1
-            #cap_color undefined
-        elif cap_color == 'W' or cap_color == 'w':
-            captured_white = captured_white + 1
+    raw_cord = input
 
-        print(f"Captured by White: {captured_black} | Captured by Black: {captured_white}")
+    #validate all of the weird things
+    #current move
+    while not all_valid:
+
+        valid = True
+
+        #add validation for making sure theres a comma
+        if valid and len(raw_cord) == 3 and raw_cord[0].isnumeric() and raw_cord[2].isnumeric() and raw_cord[1] == ",":
+            print("Whoops, please format your coordinates correctly (ex: #,#)!")
+            raw_cord = input("Please enter in the current coordinates of the piece you would like to place in play (ex: 2,4): ")
+            #this splits the inputted coordinates by the comma, which is there for user visual-convience
+            valid = False
+        else:
+            cord = piece.split(',')
+            col = cord[0]
+            row = cord[1] 
+
+        #validation that the piece they wish to play is actually on the board
+        if valid and (col < 0 or col > 7 or row < 0 or col > 7):
+            valid = False
+            print("Whoops! Eyes on the board!")
+            raw_cord = input("Please enter in the current coordinates of the piece you would like to place in play (ex: 2,4): ")
+
+        #if the value == 0, empty space bad
+        if valid and board[row][col] == 0:
+            valid = False
+            print("Please select a piece to put in play.")
+            raw_cord = input("Please enter in the current coordinates of the piece you would like to place in play (ex: 2,4): ")
+        else:
+            piece = board[row][col]
+
+        if valid and piece.get_color() != player_color:
+            valid = False
+            print("Unfair advantage! Play one of your own pieces please.")
+            raw_cord = input("Please enter in the current coordinates of the piece you would like to place in play (ex: 2,4): ")
         
-
-def current_position_validation(raw_cord, board):
-        piece = 0
-        all_valid = False
-
-        #validate all of the weird things
-        #current move
-        while not all_valid:
-
-            valid = True
-
-            #add validation for making sure theres a comma
-            if valid and len(new_move) == 3 and new_move[0].isnumeric() and new_move[2].isnumeric() and new_move[1] == ",":
-                print("Whoops, please format your coordinates correctly (ex: #,#)!")
-                raw_cord = input("Please enter in the current coordinates of the piece you would like to place in play (ex: 2,4): ")
-                #this splits the inputted coordinates by the comma, which is there for user visual-convience
-                valid = False
-            else:
-                cord = piece.split(',')
-                col = cord[0]
-                row = cord[1] 
-
-            #validation that the piece they wish to play is actually on the board
-            if valid and (col < 0 or col > 7 or row < 0 or col > 7):
-                valid = False
-                print("Whoops! Eyes on the board!")
-                raw_cord = input("Please enter in the current coordinates of the piece you would like to place in play (ex: 2,4): ")
-
-            #if the value == 0, empty space bad
-            if valid and board[row][col] == 0:
-                valid = False
-                print("Please select a piece to put in play.")
-                raw_cord = input("Please enter in the current coordinates of the piece you would like to place in play (ex: 2,4): ")
-            else:
-                piece = board[row][col]
-
-            if valid and piece.get_color() != player_color:
-                valid = False
-                print("Unfair advantage! Play one of your own pieces please.")
-                raw_cord = input("Please enter in the current coordinates of the piece you would like to place in play (ex: 2,4): ")
-            
-            if valid:
-                all_valid = True
+        if valid:
+            all_valid = True
 
     return piece, row, col
 
-def new_move_validation(new_move, piece, board):
-        all_valid = False
+def new_move_validation(input, piece, board):
+    all_valid = False
 
-        while not all_valid:
+    new_move = input
 
-            valid = True
+    while not all_valid:
 
-            #add validation for making sure theres a comma
-            if valid and len(new_move) == 3 and new_move[0].isnumeric() and new_move[2].isnumeric() and new_move[1] == ",":
-                print("Whoops, please format your coordinates correctly (ex: #,#)!") 
-                new_move = input("Please enter the coordinates for the space you would like to move the selected piece to (ex: 1,6): ")
+        valid = True
+
+        #add validation for making sure theres a comma
+        if valid and len(new_move) == 3 and new_move[0].isnumeric() and new_move[2].isnumeric() and new_move[1] == ",":
+            print("Whoops, please format your coordinates correctly (ex: #,#)!") 
+            new_move = input("Please enter the coordinates for the space you would like to move the selected piece to (ex: 1,6): ")
+            valid = False
+        else:
+            new_cord = new_move.split(',')
+            new_col = new_cord[0]
+            new_row = new_cord[1]
+
+        if valid and (new_col < 0 or new_col > 7 or new_row < 0 or new_col > 7):
+            valid = False
+            print("Whoops! Keep the pieces on the board!")
+            new_move = input("Please enter the coordinates for the space you would like to move the selected piece to (ex: 1,6): ")
+
+        if valid:
+            #after asking for new move validate the move is possible for the piece they selected
+            move_validate = piece.validate_move(new_col, new_row, board)
+            if move_validate == False:
                 valid = False
+                print("That move is not valid for that piece, please try again.")
+                new_move = input("Please enter the coordinates for the space you would like to move the selected piece to (ex: 1,6): ")
+                #add validation for making sure theres a comma
             else:
-                new_cord = new_move.split(',')
-                new_col = new_cord[0]
-                new_row = new_cord[1]
-
-            if valid and (new_col < 0 or new_col > 7 or new_row < 0 or new_col > 7):
-                valid = False
-                print("Whoops! Keep the pieces on the board!")
-                new_move = input("Please enter the coordinates for the space you would like to move the selected piece to (ex: 1,6): ")
-
-            if valid:
-                #after asking for new move validate the move is possible for the piece they selected
-                move_validate = piece.validate_move(new_col, new_row, board)
-                if move_validate == False:
-                    valid = False
-                    print("That move is not valid for that piece, please try again.")
-                    new_move = input("Please enter the coordinates for the space you would like to move the selected piece to (ex: 1,6): ")
-                    #add validation for making sure theres a comma
-                else:
-                    all_valid = True
+                all_valid = True
 
     return 
 
